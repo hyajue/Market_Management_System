1aa78b65642a39eb2a39be9efdc92d1fa0241166
Jill Li <jillli@users.noreply.github.com>
2017-07-02 20:07:07 -0700

    7/2/17

---
+++ src/Register.java
@@ -0,0 +1,23 @@
+package managmentsystem;
+
+import com.borland.dx.sql.dataset.*;
+import java.sql.*;
+import javax.swing.*;
+
+public class Register {
+  public Register( String userName , int userAge , String userGender , String userAuthority ,String userAddress ,String userPassword , String testPassword ){
+    if(userPassword.equals(testPassword)){
+      try{
+        Database database1 = new Database();
+         database1.setConnection(new com.borland.dx.sql.dataset.ConnectionDescriptor("jdbc:odbc:gaokaiSQL", "sa", "", false, "sun.jdbc.odbc.JdbcOdbcDriver"));
+        Statement sql1 = database1.createStatement() ;
+        JOptionPane.showMessageDialog(null,"注册成功,请重新登陆","",JOptionPane.DEFAULT_OPTION);
+        sql1.executeUpdate("INSERT INTO [UML_CS].[dbo].[user](userName, userAge , userGender, userAuthority , userAddress , userPassword) VALUES('"+userName+"' , '"+userAge+"' , '"+userGender+"' , '"+userAuthority+"' , '"+userAddress+"','"+userPassword+"')") ;
+      }
+      catch(SQLException www){www.printStackTrace();}
+    }
+    else{
+      JOptionPane.showMessageDialog(null,"您输入的密码前后不符,请重新输入","",JOptionPane.DEFAULT_OPTION);
+    }
+  }
+}
